f5e7783ff06c276799279f9f6acb5239fb9b8850
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-01-07 16:26:31 +0900

    first commit

---
+++ src/main/groovy/org/example/redisinspring/RedisInSpringApplication.groovy
@@ -0,0 +1,13 @@
+package org.example.redisinspring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RedisInSpringApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(RedisInSpringApplication, args)
+    }
+
+}
