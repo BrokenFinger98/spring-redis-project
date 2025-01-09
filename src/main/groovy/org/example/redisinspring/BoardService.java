867a22edf14c606e4f0fcf0724a25f3785c43b80
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-01-09 13:27:37 +0900

    refactor: cache 미적용

--- src/main/groovy/org/example/redisinspring/BoardService.java
+++ src/main/groovy/org/example/redisinspring/BoardService.java
@@ -18,7 +18,7 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
-    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #p0 + ':size:' + #p1", cacheManager = "boardCacheManager")
+//    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #p0 + ':size:' + #p1", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
